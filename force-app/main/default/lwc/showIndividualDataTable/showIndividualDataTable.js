516b4aa499aaa0412c90b519c5a58fbb6ab9160b
manpreetsfdc <manpreet.bagga@autodesk.com>
2021-08-20 01:52:00 +0530

    Covid-19 LWC component update

--- force-app/main/default/lwc/showIndividualDataTable/showIndividualDataTable.js
+++ force-app/main/default/lwc/showIndividualDataTable/showIndividualDataTable.js
@@ -1,3 +1,16 @@
+
+/**
+* ─────────────────────────────────────────────────────────────────────────────────────────────────┐
+* Component:  showIndividiualDataTable.js
+* Description: Javascript Controller that interacts with Apex class, fetches data, hnadles and perform different events.
+* ──────────────────────────────────────────────────────────────────────────────────────────────────
+* @Developer Manpreet Singh 
+* @created    2021-05-20
+* ─────────────────────────────────────────────────────────────────────────────────────────────────
+/**
+*************************************************************************************************************************/
+
+
 import { LightningElement, wire,track} from 'lwc';
 import fetchIndividuals from '@salesforce/apex/IndividualDataController.fetchIndividuals';
 import findVacCenter from '@salesforce/apex/IndividualDataController.findvacinationCenter';
@@ -35,123 +48,127 @@ columns = columns;
 rowId;
 message;
 notification;
+
 /** Wired Apex result so it can be refreshed programmatically */
 wiredIndividualssResult;
+/**
+
+*  @description      : get Individual object data
+*  @name             : fetchIndividuals
+*  @throws exception : NONE
+**/
 
 @wire( fetchIndividuals )  
-
 wiredIndividuals( result  ) {
-    this.wiredIndividualssResult = result;
-    if ( result.data ) {
-   
-    this.individuals = result.data;
-    this.error = undefined;
-
+this.wiredIndividualssResult = result;
+if ( result.data ) {
+this.individuals = result.data;
+this.error = undefined;
 } else if ( result.error ) {
-
-    this.error = result.error;
-    this.individuals = undefined;
-
+this.error = result.error;
+this.individuals = undefined;
+}
 }
 
-}
-/*
-wiredindividuals(result) {
-    console.log('@@@@@@@@@@result',result );
-    this.wiredIndividualssResult = result;
-    if (result.data) {
-        this.individuals = result.data;
-        this.error = undefined;
-    } else if (result.error) {
-        this.error = result.error;
-        this.individuals = undefined;
-    }
-}
-*/
+/**
 
-
+*  @description      : handling Find Vaccine Center RowAction
+*  @name             : findVaccinationCenter
+*  @param            : event
+*  @throws exception : NONE
+**/
 findVaccinationCenter(event){
-console.log('event.Id', JSON.stringify(event ));
 this.rowId = event.Id;  
-console.log('@@@@@@@@@@@@@rowID', this.rowId );
-    findVacCenter({recordId: this.rowId})
-    .then(result => {
-        this.message = 'Nearest Vaccination Center is- '+ ''+result;
-        console.log('@@@@@message', this.message );
-        this.showToast();
-        return refreshApex(this.wiredIndividualssResult);
-    })
-    .catch(error => {
-        this.error = error;
-        console.log(error);
-    });      
+findVacCenter({recordId: this.rowId})
+.then(result => {
+this.message = 'Nearest Vaccination Center is- '+ ''+result;
+this.showToast();
+return refreshApex(this.wiredIndividualssResult);
+})
+.catch(error => {
+this.error = error;
+console.log(error);
+});      
 }
 
+/**
+
+*  @description      : handling sendNotification RowActions
+*  @name             : sendEmailNotification
+*  @param            : event
+*  @throws exception : NONE
+**/
 sendEmailNotification(event){
-console.log('event.Id', JSON.stringify(event ));
 this.rowId = event.Id;  
-console.log('@@@@@@@@@@@@@rowID', this.rowId );
 sendEmail({recordId: this.rowId})
-    .then(result => {
-        this.notification = result;
-        this.message = 'Email Notification has been sent Sucessfully!';
-        console.log('@@@@@message', this.notification );
-        this.showToast();
-        return refreshApex(this.wiredIndividualssResult);
-    })
-    .catch(error => {
-        this.error = error;
-        console.log(error);
-    });      
+.then(result => {
+this.notification = result;
+this.message = 'Email Notification has been sent Sucessfully!';
+console.log('@@@@@message', this.notification );
+this.showToast();
+return refreshApex(this.wiredIndividualssResult);
+})
+.catch(error => {
+this.error = error;
+console.log(error);
+});      
 }
 
 
+/**
 
+*  @description      : get UI Theme 
+*  @name             : getUIThemeDescriptionName
+*  @param            : String
+*  @throws exception : NONE
+**/
 handleRowAction( event ) {
-
 const actionName = event.detail.action.name;
 const row = event.detail.row;
 switch ( actionName ) {
-    case 'view':
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: row.Id,
-                actionName: 'view'
-            }
-        });
-        break;
-    case 'edit':
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: row.Id,
-                objectApiName: 'Individual__c',
-                actionName: 'edit'
-            }
-        });
-        break;
-        case 'findvaccinationcenter':
-            console.log('calling findVaccinationCenter');
-        this.findVaccinationCenter(row);
-        break;
-        case 'SendNotification' :
-            this.sendEmailNotification(row);
-            console.log('caling sendEmailNotification');
-            
-    default:
+case 'view':
+this[NavigationMixin.Navigate]({
+type: 'standard__recordPage',
+attributes: {
+    recordId: row.Id,
+    actionName: 'view'
 }
+});
+break;
+case 'edit':
+this[NavigationMixin.Navigate]({
+type: 'standard__recordPage',
+attributes: {
+    recordId: row.Id,
+    objectApiName: 'Individual__c',
+    actionName: 'edit'
+}
+});
+break;
+case 'findvaccinationcenter':
+console.log('calling findVaccinationCenter');
+this.findVaccinationCenter(row);
+break;
+case 'SendNotification' :
+this.sendEmailNotification(row);
+console.log('caling sendEmailNotification');
 
+default:
 }
+}
+/**
+
+*  @description      : get UI Theme 
+*  @name             : getUIThemeDescriptionName
+*  @param            : String
+*  @throws exception : NONE
+**/
 showToast() {
-console.log()
 const event = new ShowToastEvent({
-    message: this.message,
-    variant: 'success',
-    mode: 'dismissable'
+message: this.message,
+variant: 'success',
+mode: 'dismissable'
 });
 this.dispatchEvent(event);
-
 }
-
 }
